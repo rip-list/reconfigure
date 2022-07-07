2ed405cc0bd2824fc4578c664a68cbd96b98f2ba
zip <64793652+rip-list@users.noreply.github.com>
2022-07-07 18:05:30 +0000

    Update len.sh

--- len.sh
+++ len.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#echo $(sudo mount -o remount,rw  / )
+#qwerty клавиатура на AltGR 
 echo $(setxkbmap -layout us,ru -option grp:toggle)
-#echo $(xrandr --output VGA-0 --mode 1366x768_60.00)
+#разрешение 
 echo $(xrandr --output DVI-I-0 --mode 1368x768_60.00 +1366)
-#echo $(./xinitrc)
+
